b67a492a7547ed81915605d118f07d49242d7fd8
Kavin M <89208380+kavinm21@users.noreply.github.com>
2022-03-08 10:58:34 +0530

    Periodic Update prog1.py

--- prog1.py
+++ prog1.py
@@ -8,23 +8,37 @@ Created on Tue Mar  8 09:02:21 2022
 import yaml
 import pandas as pd
 import datetime as dt
+import time
 
+#function to create a log line
+def log_line(str_template):
+    string = str(dt.datetime.now())
+    for x in str_template:
+        string += x
+    return string
+#function for time_function
+def time_function(exec_time):
+    #pause execution
+    time.sleep(exec_time)
 
-def time_function(curr_time, exec_time):
-    curr_time = curr_time + dt.timedelta(seconds=exec_time)
+#here the activities are parsed
+def act_on_activities(activity, txt_lines):
     
-def act_on_activities(activity, curr_time):
-    lines = []
     #parsing the activities to execute them
     list_tasks = list(activity.keys())
+    act_template = str_template + ""
     for x in list_tasks:
         task = activity[x]
+        str_template.append(x)
+        line = str(dt.datetime.now()) + str_template[0]
+        line += str_template[1] + "Entry"
+        txt_line.append()
         task_attr = list(task.keys())
         if "Execution" in task_attr:
             sub_flow = task['Activities']
         else:
             if task['Function'] == 'TimeFunction':
-                time_function(curr_time, exec_time)
+                time_function(exec_time)
                 op_str = str(curr_time) + ';' + 
                 lines.append()
                 
@@ -35,18 +49,18 @@ if __name__ == "__main__":
             ip_data = yaml.load(ip_file, Loader=yaml.FullLoader)
             log_file = open("Milestone1_Example.txt", "a+")
             workflow_keys = list(ip_data.keys())
-            task_string = str(curr_time) + ';' + x + "Entry"
-            txt_lines.append(task_string)
             for x in workflow_keys:
+                txt_lines = []
+                str_template = [x]
+                task_string = log_line(str_template) + " Entry"
+                txt_lines.append(task_string)
                 workflow = ip_data['M1SampleWorkFlow']
-                type_exec = workflow['Type']
+                #type_exec = workflow['Type']
                 exec_ord = workflow['Execution']
                 activity = workflow['Activities']
-                curr_time = dt.datetime.now()
-                txt_lines = []
-                act_on_activities(txt_lines, activity, curr_time)
-                
+                act_on_activities(activity, str_template, txt_lines)
+                task_string = log_line(str_template) + "Exit"
                 log_file.writelines(txt_lines)
-            
+                
             
             
