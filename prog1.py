7fe716563b3f1fa86bb387caeb5bef9c48cfe8ec
Kavin M <89208380+kavinm21@users.noreply.github.com>
2022-03-08 09:48:11 +0530

    Files recieved and prog

---
+++ prog1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  8 09:02:21 2022
+
+@author: kavin
+"""
+
+import yaml
+import pandas as pd
+
+
+if __name__ == "__main__":
+    
+    with open("Files\Examples\Milestone1\Milestone1_Example.yaml") \
+    as ip_file:
+            ip_data = yaml.load(ip_file, Loader=yaml.FullLoader)
+            workflow_keys = ip_data.keys()
+            workflow = ip_data['M1SampleWorkFlow']
+            type_exec = workflow['Type']
+            exec_ord = workflow['Execution']
+            
+            
