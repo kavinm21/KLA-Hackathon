045932da0b240876d11c5606a1ae268d51059be5
Kavin M <89208380+kavinm21@users.noreply.github.com>
2022-03-08 10:19:27 +0530

    Update prog1.py

--- prog1.py
+++ prog1.py
@@ -7,12 +7,26 @@ Created on Tue Mar  8 09:02:21 2022
 
 import yaml
 import pandas as pd
+import datetime as dt
 
-def act_on_activities(activity):
-    list_tasks = activity.keys()
-    print(list_tasks)
+
+def time_function(curr_time, exec_time):
+    curr_time = curr_time + dt.timedelta(seconds=exec_time)
     
-    
+def act_on_activities(activity, curr_time):
+    lines = []
+    list_tasks = list(activity.keys())
+    for x in list_tasks:
+        task = activity[x]
+        task_attr = list(task.keys())
+        if "Execution" in task_attr:
+            sub_flow = task['Activities']
+        else:
+            if task['Function'] == 'TimeFunction':
+                time_function(curr_time, exec_time)
+                op_str = str()
+                lines.append()
+                
 if __name__ == "__main__":
     
     with open("Files\Examples\Milestone1\Milestone1_Example.yaml") \
@@ -23,5 +37,10 @@ if __name__ == "__main__":
             type_exec = workflow['Type']
             exec_ord = workflow['Execution']
             activity = workflow['Activities']
-            act_on_activities(activity)
+            curr_time = dt.datetime.now()
+            txt_lines = act_on_activities(activity, curr_time)
+            log_file = open("Milestone1_Example.txt")
+            log_file.writelines(txt_lines)
+            
+            
             
